b9f14af3e6de1faf08aa4a44d8184534cba69900
Dmytro Sych <dmytrosych834@gmail.com>
2018-05-25 13:35:46 -0400

    splitting install into a module

--- tools/install.js
+++ tools/install.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
+const commandRunner = require('./run-command');
 
 const install = 'npm install';
-// decend into editor
-process.chdir('src/editor');
-const p = exec(install).on('exit', code => {
+commandRunner(install, 'src/editor', code => {
     if (!!code) {
         throw 'Unable to install editor dependencies.';
     }
 });
-logOutput(p);
-
-function logOutput(p) {
-    p.stdout.on('data', output => {
-        process.stdout.write(output.toString());
-    });
-    p.stderr.on('data', output => {
-        process.stderr.write(output.toString());
-    });
-}
