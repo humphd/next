3ca65e01945591abab093a49c6e9c87b310457d3
Dmytro <dsych@users.noreply.github.com>
2018-05-30 12:59:46 -0400

    Indexed api. fixes #25 (#41)
    * working on service worker communication with cached files
    
    * finishing api
    
    * adding hot cache reload support
    
    * fixing cache route substituition
    
    * deployment
    
    * changing domain
    
    * adding db
    
    * splitting url and body. allowing es2017 syntax
    
    * adding full support for POST API.
    
    * adding get logic
    
    * finishing upport for native promises in es7
    
    * adding support for bulk retrieve
    
    * limiting travis deployement to latest version on node
    
    * enabling delete and adding docs
    
    * updating dependancies
    
    * Update db-worker.js
    
    Added basic routing stubs
    
    * updating dependencies for precache
    
    * Syncing with upstream
    
    * splitting db from the root repo
    
    * cleaning up the dev data
    
    * adding proper versioning for db and optimizing sw
    
    * removing cache gap

---
+++ src/db/process.js
@@ -0,0 +1,58 @@
+const submitForm = () => {
+    const urlElement = window.document.getElementById('url');
+    const bodyElement = window.document.getElementById('body');
+    const methodElement = document.getElementById('method');
+
+    let url = `${window.location.hostname}/data/api/`;
+
+    let body =
+        // @ts-ignore
+        methodElement.value === 'GET' || methodElement.value === 'DELETE'
+            ? null
+            : // @ts-ignore
+              JSON.stringify(bodyElement.value);
+    // @ts-ignore
+    url += urlElement.value;
+
+    // @ts-ignore
+    console.log(methodElement.value);
+
+    const req = new Request(encodeURI(url), {
+        // @ts-ignore
+        method: methodElement.value,
+        body: body,
+    });
+
+    fetch(req)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            document.getElementById('result').innerText = JSON.stringify(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    // @ts-ignore
+    console.log(urlElement.value);
+};
+
+window.addEventListener('load', () => {
+    document
+        .getElementById('submitButton')
+        .addEventListener('click', submitForm, false);
+    document.getElementById('method').addEventListener('change', revealMethod);
+
+    revealMethod({
+        target: { value: document.getElementById('method').value },
+    });
+});
+
+function revealMethod(event) {
+    // @ts-ignore
+    if (event.target.value == 'GET' || event.target.value == 'DELETE') {
+        document.getElementById('bodyWrapper').hidden = true;
+    } else {
+        document.getElementById('bodyWrapper').hidden = false;
+    }
+}
