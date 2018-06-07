5b6e2c48ee1f3dba7d9a226be969ce20c72198d3
Mat Babol <mmbabol@gmail.com>
2018-06-07 17:42:41 -0400

    Created script for installation

--- tools/build.js
+++ tools/build.js
@@ -5,17 +5,47 @@ const commandRunner = require('./run-command');
 const build = 'npm run build';
 const build_db = 'npm run build -- --public-url=/"$GH-PAGES/"';
 
-commandRunner('mkdir -p deploy && rm -R deploy/', '.')
-    .then(() => commandRunner(build, 'src/docs/website'))
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+var copy = require('copy');
+
+rimraf('./dist', {}, function() {});
+
+mkdirp('dist', function(err) {
+    if (err) console.error(err);
+    else console.log('Created dist directory');
+});
+
+commandRunner(build, 'src/docs/website')
     .then(() =>
-        commandRunner('mv -vf ./src/docs/website/build/Next/ deploy', '.')
+        copy('./src/docs/website/build/Next/**', './dist', function(err, file) {
+            if (err) console.error(err);
+        })
     )
-    .then(() => commandRunner('mkdir -p deploy/editor', '.'))
+    .then(() =>
+        mkdirp('dist/editor', function(err) {
+            if (err) console.error(err);
+            else console.log('Created dist/editor directory');
+        })
+    )
     .then(() => commandRunner(build, 'src/editor'))
-    .then(() => commandRunner('mv -vf ./src/editor/dist deploy/editor', '.'))
-    .then(() => commandRunner('mkdir -p deploy/dist', '.'))
+    .then(() =>
+        copy('./src/editor/dist/**', './dist/edtor', function(err, file) {
+            if (err) console.error(err);
+        })
+    )
+    .then(() =>
+        mkdirp('dist/dist', function(err) {
+            if (err) console.error(err);
+            else console.log('Created src/editor directory');
+        })
+    )
     .then(() => commandRunner(build_db, 'src/db'))
-    .then(() => commandRunner('mv -vf ./src/db/dist deploy', '.'))
+    .then(() =>
+        copy('./src/db/dist/**', './dist/dist', function(err, file) {
+            if (err) console.error(err);
+        })
+    )
     .catch(err => {
         throw `Unable to build ${err.message}`;
     });
