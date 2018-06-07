5ca6e44cc9d540868471fc626f9c3285f6c50adc
David Humphrey <david.humphrey@senecacollege.ca>
2018-06-07 12:32:01 -0400

    Fixes for travis and npm

--- tools/install.js
+++ tools/install.js
@@ -4,8 +4,7 @@ const commandRunner = require('./run-command');
 
 const install = 'npm install';
 
-commandRunner(install, 'src/db')
-    .then(() => commandRunner(install, 'src/editor'))
+commandRunner(install, 'src/editor')
     .then(() => commandRunner(install, 'src/docs/website'))
     .catch(err => {
         throw `Unable to install dependencies ${err.message}`;
