1b470666071eb65c4b40d38fb285624e4d52e2c9
Mat Babol <mmbabol@gmail.com>
2018-06-08 11:26:37 -0400

    Fix build script

--- tools/build.js
+++ tools/build.js
@@ -1,51 +1,19 @@
 #!/usr/bin/env node
 
 const commandRunner = require('./run-command');
+const util = require('util');
 
-const build = 'npm run build';
-const build_db = 'npm run build -- --public-url=/"$GH-PAGES/"';
+var mkdirp = util.promisify(require('mkdirp'));
+var rimraf = util.promisify(require('rimraf'));
+var copy = util.promisify(require('copy'));
 
-var mkdirp = require('mkdirp');
-var rimraf = require('rimraf');
-var copy = require('copy');
-
-rimraf('./dist', {}, function() {});
-
-mkdirp('dist', function(err) {
-    if (err) console.error(err);
-    else console.log('Created dist directory');
-});
-
-commandRunner(build, 'src/docs/website')
-    .then(() =>
-        copy('./src/docs/website/build/Next/**', './dist', function(err, file) {
-            if (err) console.error(err);
-        })
-    )
-    .then(() =>
-        mkdirp('dist/editor', function(err) {
-            if (err) console.error(err);
-            else console.log('Created dist/editor directory');
-        })
-    )
-    .then(() => commandRunner(build, 'src/editor'))
-    .then(() =>
-        copy('./src/editor/dist/**', './dist/edtor', function(err, file) {
-            if (err) console.error(err);
-        })
-    )
-    .then(() =>
-        mkdirp('dist/dist', function(err) {
-            if (err) console.error(err);
-            else console.log('Created src/editor directory');
-        })
-    )
-    .then(() => commandRunner(build_db, 'src/db'))
-    .then(() =>
-        copy('./src/db/dist/**', './dist/dist', function(err, file) {
-            if (err) console.error(err);
-        })
-    )
+mkdirp('dist')
+    .then(() => commandRunner('npm run build-docs'))
+    .then(() => copy('./src/docs/website/build/Next/**', './dist'))
+    .then(() => commandRunner('npm run build-editor'))
+    .then(() => mkdirp('dist/editor'))
+    .then(() => copy('./src/editor/dist/**', './dist/editor'))
+    .then(() => commandRunner('npm run build-terminal'))
     .catch(err => {
-        throw `Unable to build ${err.message}`;
+        throw err;
     });
