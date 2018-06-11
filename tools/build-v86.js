1fa9c4a78656bf8168f56882ee0e535244d4f81c
David Humphrey <david.humphrey@senecacollege.ca>
2018-06-11 10:47:36 -0400

    Add v86:bash npm script for manually configuring buildroot

--- tools/build-v86.js
+++ tools/build-v86.js
@@ -8,7 +8,7 @@ const v86defconfigDir = path.join(root, 'buildroot-v86');
 const v86outDir = path.join(root, 'v86-out');
 
 // docker run --rm --name build-v86 -v $PWD/v86-out:/build -v $PWD:/buildroot-v86 buildroot
-const args = [
+let args = [
     'run',
     '--rm',    
     '--name',
@@ -16,9 +16,17 @@ const args = [
     '-v',
     `${v86outDir}:/build`,
     '-v',
-    `${v86defconfigDir}:/buildroot-v86`,
-    'buildroot'
+    `${v86defconfigDir}:/buildroot-v86`
 ];
+
+// Pass any extra args onto docker (e.g., --entrypoint ...)
+if(process.argv.length >= 3) {
+    args = args.concat(process.argv.slice(2));
+}
+
+args = args.concat('buildroot');
+
+console.log('docker', args.join(' '));
 
 const options = {
     cwd: root,
