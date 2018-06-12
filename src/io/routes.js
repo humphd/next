eae6df7734bd02ca459a3db9d52802e6718b2f6b
Jerry Goguette <jerry_g16@hotmail.com>
2018-06-12 19:43:09 -0400

    Added Recursive folder creation

--- src/io/routes.js
+++ src/io/routes.js
@@ -9,17 +9,32 @@ export default (workbox, ioServer) => {
         ioInRegex,
         async ({ url }) => {
             const path = url.pathname.match(ioInRegex)[1];
-            let body;
-            let type;
-            let status;
+            let body, type, status, result;
             try {
-                // const result1 = await ioServer.createPath();
-                const result = await ioServer.serve(path);
+                let pathExist;
+                // Check if path exist
+                await ioServer
+                    .dirExists(path)
+                    .then(exist => {
+                        pathExist = exist;
+                    })
+                    .catch(exist => {
+                        pathExist = exist;
+                    });
+
+                if (pathExist) {
+                    result = await ioServer.serve(path);
+                } else {
+                    // If not, create full path then serve
+                    await ioServer.createPathRecursive(path);
+                    result = await ioServer.serve(path);
+                }
+
                 body = result.body;
                 type = result.type;
                 status = 200;
             } catch (err) {
-                body = format404(path);
+                body = err;
                 type = 'text/html';
                 // TODO: should probably do a better job here on mapping to err
                 status = 404;
@@ -39,32 +54,33 @@ export default (workbox, ioServer) => {
     workbox.routing.registerRoute(
         ioImportRegex,
         async ({ url, event, params }) => {
-            return event.request.formData().then(async formData => {
-                let body;
-                let status;
-                let type;
-                try {
-                    var files = JSON.parse(formData.get('file'));
-                    const result = await ioServer.uploadFiles(files);
-                    body = result.body;
-                    status = 200;
-                } catch (err) {
-                    body = err;
-                    type = 'text/html';
-                    // TODO: should probably do a better job here on mapping to err
-                    status = 404;
-                }
+            return event.request
+                .formData()
+                .then(async formData => {
+                    let body,
+                        status,
+                        type = 'text/html';
+                    try {
+                        var files = JSON.parse(formData.get('file'));
+                        const result = await ioServer.uploadFiles(files);
+                        body = JSON.stringify(result);
+                        status = 200;
+                    } catch (err) {
+                        body = err;
+                        status = 404;
+                    }
 
-                const init = {
-                    status,
-                    statusText: 'OK',
-                    headers: { 'Content-Type': type },
-                };
-    
-                // return new Response(body, init);
-            }).catch(err => { return new Response(
-                err
-            ); });
+                    const init = {
+                        status,
+                        statusText: 'OK',
+                        headers: { 'Content-Type': type },
+                    };
+
+                    return new Response(body, init);
+                })
+                .catch(err => {
+                    return new Response(err);
+                });
         },
         'POST'
     );
