e8227bd7d721eb8cf40c97f6e3177950e1574b9a
Jerry Goguette <jerry_g16@hotmail.com>
2018-06-14 18:29:47 -0400

    Changes

--- src/io/html-formatter.js
+++ src/io/html-formatter.js
@@ -12,19 +12,7 @@ export const format404 = url => {
             <hr></body></html>`;
 };
 
-/**
- * Send an Apache-style directory listing
- */
-export const formatDir = (dirPath, entries) => {
-    const parent = path.dirname(dirPath);
-
-    const header = `<!DOCTYPE html>
-                    <html>
-                        <head>
-                            <title>Index of IO</title>
-                            <link href="/css/styles.css" rel="stylesheet"/>
-                        </head>`;
-
+export const formatEntries = (dirPath, entries) => {
     var len = entries.length,
         output = [];
     for (var i = 0; i < len; i++) {
@@ -49,42 +37,5 @@ export const formatDir = (dirPath, entries) => {
         output.push(JSON.stringify(entry));
     }
 
-    const body = `  <body>
-                        <div class="filemanager">
-                            <div class="breadcrumbs"></div>
-                            <ul class="data"></ul>
-                            <div class="nothingfound">
-                                <div class="nofiles"></div>
-                                <span>No files here.</span>
-                            </div>
-                            </br>
-                            <div class="container">
-                                <form class="box" method="post" action="/io/import" style="display: none;" enctype="multipart/form-data" id="uploadform">
-                                    <div class="box__input">
-                                        <input class="box__file" type="file" name="files[]" id="file" data-multiple-caption="{count} files selected" multiple />
-                                        <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
-                                        <button type="submit" class="box__button">Upload</button>
-                                    </div>
-                                    <div class="box__uploading">Uploading&hellip;</div>
-                                    <div class="box__success">Done!</div>
-                                    <div class="box__error">Error(s): <span></span>.</div>
-                                </form>
-                            </div>
-                        </div>
-                        <script> 
-                            let entries = '${output}';
-                            if ( entries != '') {
-                                entries = entries
-                                            .split('},')
-                                            .map(entry => { if (entry.substr(-1) != '}') entry += '}'; return JSON.parse(entry) });
-                            }
-                            console.table(entries);
-                        </script>
-                        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-                        <script src="/scripts/script.js"></script>
-                    </body>`;
-
-    const footer = `</html>`;
-
-    return header + body + footer;
-};
+    return output;
+}
