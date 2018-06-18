6718f11118920349ba5ea0f8df0e91d2be131748
Dmytro <dmytrosych834@gmail.com>
2018-06-18 15:34:48 -0400

    merging /www and /www/json routes

--- src/web-server/html-formatter.js
+++ src/web-server/html-formatter.js
@@ -1,4 +1,4 @@
-import { isMedia, isImage } from './content-type';
+import { isMedia, isImage, getMimeType } from './content-type';
 import iconImage from './icons/image2.png';
 import iconFolder from './icons/folder.png';
 import iconMovie from './icons/movie.png';
@@ -57,82 +57,89 @@ const formatRow = (
 };
 
 const footerClose = '<address>nohost/0.0.2 (Web)</address></body></html>';
+export default class {
+    /**
+     * Send an Apache-style 404
+     */
+    format404(url) {
+        return {
+            body: `<!DOCTYPE html>
+                <html><head>
+                <title>404 Not Found</title>
+                </head><body>
+                <h1>Not Found</h1>
+                <p>The requested URL ${url} was not found on this server.</p>
+                <hr>${footerClose}`,
+            type: 'text/html',
+            status: 404,
+        };
+    }
 
-/**
- * Send an Apache-style 404
- */
-export const format404 = url => {
-    return {
-        body: `<!DOCTYPE html>
-            <html><head>
-            <title>404 Not Found</title>
-            </head><body>
-            <h1>Not Found</h1>
-            <p>The requested URL ${url} was not found on this server.</p>
-            <hr>${footerClose}`,
-        type: 'text/html',
-    };
-};
+    /**
+     * Send an Apache-style directory listing
+     */
+    formatDir(dirPath, entries) {
+        const parent = path.dirname(dirPath);
 
-export const format404AsJson = url => {
-    return {
-        body: `The requested URL ${url} was not found on this server.`,
-        type: 'application/json',
-    };
-};
+        const header = `<!DOCTYPE html>
+                        <html><head><title>Index of ${dirPath}</title></head>
+                        <body><h1>Index of ${dirPath}</h1>
+                        <table><tr><th><img src="${iconBlank}" alt="[ICO]"></th>
+                        <th><b>Name</b></th><th><b>Last modified</b></th>
+                        <th><b>Size</b></th><th><b>Description</b></th></tr>
+                        <tr><th colspan="5"><hr></th></tr>
+                        <tr><td valign="top"><img src="${iconBack}" alt="[DIR]"></td>
+                        <td><a href="/www${parent}">Parent Directory</a></td><td>&nbsp;</td>
+                        <td align="right">  - </td><td>&nbsp;</td></tr>`;
 
-/**
- * Send an Apache-style directory listing
- */
-export const formatDir = (dirPath, entries) => {
-    const parent = path.dirname(dirPath);
+        const footer = `<tr><th colspan="5"><hr></th></tr></table>${footerClose}`;
 
-    const header = `<!DOCTYPE html>
-                    <html><head><title>Index of ${dirPath}</title></head>
-                    <body><h1>Index of ${dirPath}</h1>
-                    <table><tr><th><img src="${iconBlank}" alt="[ICO]"></th>
-                    <th><b>Name</b></th><th><b>Last modified</b></th>
-                    <th><b>Size</b></th><th><b>Description</b></th></tr>
-                    <tr><th colspan="5"><hr></th></tr>
-                    <tr><td valign="top"><img src="${iconBack}" alt="[DIR]"></td>
-                    <td><a href="/www${parent}">Parent Directory</a></td><td>&nbsp;</td>
-                    <td align="right">  - </td><td>&nbsp;</td></tr>`;
+        const rows = entries
+            .map(entry => {
+                const ext = path.extname(entry.name);
+                const href = '/www' + path.join(dirPath, entry.name);
+                let icon;
+                let alt;
 
-    const footer = `<tr><th colspan="5"><hr></th></tr></table>${footerClose}`;
-
-    const rows = entries
-        .map(entry => {
-            const ext = path.extname(entry.name);
-            const href = '/www' + path.join(dirPath, entry.name);
-            let icon;
-            let alt;
-
-            if (entry.type === 'DIRECTORY') {
-                icon = iconFolder;
-                alt = '[DIR]';
-            } else {
-                if (isImage(ext)) {
-                    icon = iconImage;
-                    alt = '[IMG]';
-                } else if (isMedia(ext)) {
-                    icon = iconMovie;
-                    alt = '[MOV]';
+                if (entry.type === 'DIRECTORY') {
+                    icon = iconFolder;
+                    alt = '[DIR]';
                 } else {
-                    icon = iconText;
-                    alt = '[TXT]';
+                    if (isImage(ext)) {
+                        icon = iconImage;
+                        alt = '[IMG]';
+                    } else if (isMedia(ext)) {
+                        icon = iconMovie;
+                        alt = '[MOV]';
+                    } else {
+                        icon = iconText;
+                        alt = '[TXT]';
+                    }
                 }
-            }
 
-            return formatRow(
-                icon,
-                alt,
-                href,
-                entry.name,
-                entry.mtime,
-                entry.size
-            );
-        })
-        .join('\n');
+                return formatRow(
+                    icon,
+                    alt,
+                    href,
+                    entry.name,
+                    entry.mtime,
+                    entry.size
+                );
+            })
+            .join('\n');
 
-    return header + rows + footer;
-};
+        return {
+            type: 'text/html',
+            status: 200,
+            body: header + rows + footer,
+        };
+    }
+
+    formatFile(path, content) {
+        return {
+            body: content,
+            type: getMimeType(path),
+            status: 200,
+        };
+    }
+}
--- src/web-server/routes.js
+++ src/web-server/routes.js
@@ -1,38 +1,9 @@
-import { format404, format404AsJson } from './html-formatter';
-import fs from '../lib/fs';
+import HtmlFormatter from './html-formatter';
+import JsonFormatter from './json-formatter';
 
 const wwwRegex = /\/www(\/.*)/;
-const wwwJsonRegex = /\/www\/json(\/.*)/;
-
-/**
- * Constructs a Response based on the success of the __func__ parameter.
- * This function assumes that return of the __func__ and __handle404__ will be an object containing __type__ and __body__.
- *
- * **E.g.**
- *
- * { type: 'html/text', body: '...'}
- * @param {*} param0 receives a __func__ reference to the main function to be called;
- *  __handle404__ function to be called in case error occured.
- */
-const constructResponse = async ({ func, handle404 }) => {
-    let res;
-    let status;
-    try {
-        res = await func();
-        status = 200;
-    } catch (err) {
-        res = handle404();
-        // TODO: should probably do a better job here on mapping to err
-        status = 404;
-    }
-
-    const config = {
-        status,
-        statusText: 'OK',
-        headers: { 'Content-Type': res.type },
-    };
-    return new Response(res.body, config);
-};
+const jsonFormatter = new JsonFormatter();
+const htmlFormatter = new HtmlFormatter();
 
 export default (workbox, webServer) => {
     // Cache service-worker icon files (PNG) in the root
@@ -41,56 +12,29 @@ export default (workbox, webServer) => {
         workbox.strategies.staleWhileRevalidate()
     );
 
-    workbox.routing.registerRoute(
-        wwwJsonRegex,
-        async ({ url }) => {
-            const path = url.pathname.match(wwwJsonRegex)[1];
-            // optionally check if we want to get file content,too
-            const getFileContent = !!url.searchParams.get('content');
-            return await constructResponse({
-                func: ((getContent, path) =>
-                    new Promise((resolve, reject) => {
-                        // run ls on the file
-                        fs.stat(path, (err, stats) => {
-                            if (err) {
-                                return reject(err);
-                            }
-                            if (getContent) {
-                                // get file content and append to response as `content` and `contentType`
-                                webServer.serve(path).then(res => {
-                                    resolve({
-                                        type: 'application/json',
-                                        body: JSON.stringify({
-                                            metadata: stats,
-                                            content: res.body,
-                                            contentType: res.type,
-                                        }),
-                                    });
-                                });
-                            } else {
-                                resolve({
-                                    type: 'application/json',
-                                    body: JSON.stringify({ metadata: stats }),
-                                });
-                            }
-                        });
-                    })).bind(null, getFileContent, path, webServer),
-                handle404: format404AsJson.bind(null, path),
-            });
-        },
-        'GET'
-    );
-
     // @ts-ignore
     workbox.routing.registerRoute(
         wwwRegex,
         async ({ url }) => {
+            const formatter =
+                url.searchParams.get('json') === 'true'
+                    ? jsonFormatter
+                    : htmlFormatter;
             const path = url.pathname.match(wwwRegex)[1];
 
-            return await constructResponse({
-                func: webServer.serve.bind(webServer, path),
-                handle404: format404.bind(null, path),
-            });
+            let res;
+            try {
+                res = await webServer.serve(path, formatter);
+            } catch (err) {
+                res = formatter.format404(path);
+            }
+
+            const config = {
+                status: res.status,
+                statusText: 'OK',
+                headers: { 'Content-Type': res.type },
+            };
+            return new Response(res.body, config);
         },
         'GET'
     );
