4e61c510105f1446b5c46946e9785f4c652c973d
Dmytro <dsych@users.noreply.github.com>
2018-06-19 17:02:47 -0400

    First release (#112)
    * updating binary paths. fixing docker volumes
    
    * fetching from latest release

--- tools/build-v86.js
+++ tools/build-v86.js
@@ -5,31 +5,34 @@ const { spawn } = require('child_process');
 
 const root = path.resolve(path.join(__dirname, '..'));
 const v86defconfigDir = path.join(root, 'buildroot-v86');
-const v86outDir = path.join(root, 'v86-out');
+let v86outDir = path.join(root, 'v86-out');
+const reg = /^(.):\\/;
+// since on windows we are running docker on guest Ubuntu vm,
+// we need to pass it path that it recognises.
+// details here: https://stackoverflow.com/a/33314404
+if (process.platform === 'win32') {
+    // extract drive letter and convert it into the form of '/letter/'
+    const matched = v86outDir.match(reg)[1].toLowerCase();
+    // replace all windows path separators e.g. '\' with unix path separators e.g. '/'
+    v86outDir = v86outDir.replace(reg, '/' + matched + '/').replace(/\\/g, '/');
+}
 
 // If you want to mount `/buildroot-v86` to reconfigure, set this
 const overrideConfigDir = process.env.OVERRIDE_CONFIG_DIR == 1;
 
 // docker run --rm --name build-v86 -v $PWD/v86-out:/build -v $PWD:/buildroot-v86 buildroot
-let args = [
-    'run',
-    '--rm',    
-    '--name',
-    'build-v86',
-    '-v',
-    `${v86outDir}:/build`
-];
+let args = ['run', '--rm', '--name', 'build-v86', '-v', `${v86outDir}:/build`];
 
 // Optionally override (via run-time mount) the /buildroot-v86 dir
-if(overrideConfigDir) {
+if (overrideConfigDir) {
     args = args.concat([
         '--mount',
-        `type=bind,source=${v86defconfigDir},target=/buildroot-v86`
+        `type=bind,source=${v86defconfigDir},target=/buildroot-v86`,
     ]);
 }
 
 // Pass any extra args onto docker (e.g., --entrypoint ...)
-if(process.argv.length >= 3) {
+if (process.argv.length >= 3) {
     args = args.concat(process.argv.slice(2));
 }
 
@@ -39,7 +42,7 @@ console.log('docker', args.join(' '));
 
 const options = {
     cwd: root,
-    stdio: 'inherit'
+    stdio: 'inherit',
 };
 
 spawn('docker', args, options);
--- tools/download-vm.js
+++ tools/download-vm.js
@@ -10,17 +10,8 @@ const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
 
-const isoUrl = 'https://copy.sh/v86/images/linux3.iso';
-const biosUrl =
-    'https://github.com/copy/v86/blob/master/bios/seabios.bin?raw=true';
-const vgaBiosUrl =
-    'https://github.com/copy/v86/blob/master/bios/vgabios.bin?raw=true';
-
 // Put all binary files in `dist/terminal/bin` and we'll cache these on travis.
 const terminalDir = path.join(__dirname, '..', 'dist', 'terminal', 'bin');
-const isoDestPath = path.join(terminalDir, 'linux3.iso');
-const biosDestPath = path.join(terminalDir, 'seabios.bin');
-const vgaBiosDest = path.join(terminalDir, 'vgabios.bin');
 
 const download = (url, dest) => {
     return new Promise((resolve, reject) => {
@@ -47,11 +38,31 @@ const download = (url, dest) => {
 mkdirp(terminalDir, err => {
     if (err) throw err;
 
-    Promise.all([
-        download(isoUrl, isoDestPath),
-        download(biosUrl, biosDestPath),
-        download(vgaBiosUrl, vgaBiosDest),
-    ])
-        .then(() => console.log('Done.'))
-        .catch(err => console.error(err));
+    request.get(
+        {
+            url: 'https://api.github.com/repos/humphd/next/releases/latest',
+            json: true,
+            headers: {
+                'User-Agent': 'Request-Promise',
+            },
+        },
+        (err, resp, release) => {
+            if (err) throw err;
+            if (resp.statusCode !== 200)
+                throw `Unable to get latest release. ${resp.statusMessage}`;
+            Promise.all(
+                release.assets.map(el =>
+                    download(
+                        el.browser_download_url,
+                        path.join(
+                            terminalDir,
+                            path.basename(el.browser_download_url)
+                        )
+                    )
+                )
+            )
+                .then(() => console.log('Done'))
+                .catch(err => console.error(err));
+        }
+    );
 });
