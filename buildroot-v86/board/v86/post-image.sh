5da22f88a74bcc9c590dbdcf7d0aa2fac1438d98
David Humphrey <david.humphrey@senecacollege.ca>
2018-06-22 21:23:53 -0400

    Fix #115: build and include license info in buildroot output

--- buildroot-v86/board/v86/post-image.sh
+++ buildroot-v86/board/v86/post-image.sh
@@ -3,5 +3,11 @@
 # Run after buildroot has built the image, and path to the built
 # output/image dir is passed as first arg.  We copy the built ISO
 # out of the container.
-cp $1/rootfs.iso9660 /build/v86-linux.iso
+cp ${BINARIES_DIR}/rootfs.iso9660 /build/v86-linux.iso
 echo "Created v86-linux.iso.\n"
+
+tar czvf /build/licenses.tar.gz \
+    ${BASE_DIR}/legal-info/buildroot.config \
+    ${BASE_DIR}/legal-info/host-licenses \
+    ${BASE_DIR}/legal-info/licenses
+echo "Created license.tar.gz"
