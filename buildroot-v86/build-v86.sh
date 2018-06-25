759d163456afbdbf71863f9af5caf72ec2497f6b
David Humphrey <david.humphrey@senecacollege.ca>
2018-06-24 21:23:13 -0400

    Fix #115: build and include license info in buildroot output (#127)
    * Fix #115: build and include license info in buildroot output
    
    * Also download and include SeaBIOS license file

--- buildroot-v86/build-v86.sh
+++ buildroot-v86/build-v86.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
-# Build our v86 defconfig
+# Build our v86 defconfig along with license files.
 echo $PWD
-make BR2_EXTERNAL=/buildroot-v86 v86_defconfig && make
+make BR2_EXTERNAL=/buildroot-v86 v86_defconfig \
+    && make legal-info \
+    && make
